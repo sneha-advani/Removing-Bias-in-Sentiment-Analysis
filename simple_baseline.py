fb1bfd190cfc9624b4d36bfbb071d51bd1e6a021
kanikamohan <38482181+kanikamohan@users.noreply.github.com>
2019-05-05 21:38:59 -0400

    gold files, edited simple_baseline

--- simple_baseline.py
+++ simple_baseline.py
@@ -11,6 +11,8 @@ parser.add_argument('--tweet_file', type=str, required=True)
 parser.add_argument('--lexicon', type=str, required=True)
 parser.add_argument('--outputfile', type=str, required=True)
 parser.add_argument('--just_numbers', type=bool, required=True)
+# parser.add_argument('--write_scores', type=bool, required=True)
+
 
 def lexicon2dict(lexicon):
     #convert affect intensity lexicon to dictionary
@@ -34,6 +36,7 @@ def tokenize_tweets(tweet_file):
     tknzr = TweetTokenizer()
     all_tweets = []
     tweet_ids = []
+    all_scores = []
     #read file, tokenize all tweets, append to list
     with open(tweet_file, 'r', encoding='utf8') as f:
         next(f) #skip the header
@@ -46,7 +49,8 @@ def tokenize_tweets(tweet_file):
             words = tknzr.tokenize(tweet)
             all_tweets.append(words)
             tweet_ids.append(ID)
-    return all_tweets, tweet_ids
+            all_scores.append(np.float64(score))
+    return all_tweets, tweet_ids, all_scores
 
 
 def get_scores(all_tweets, affect_dict):
@@ -96,9 +100,12 @@ python simple_baseline.py --tweet_file final_data_files/tweets-train.txt --lexic
 def main(args):
     print(args.tweet_file)
     affect_dict = lexicon2dict(args.lexicon)
-    all_tweets, tweet_ids = tokenize_tweets(args.tweet_file)
+    all_tweets, tweet_ids, all_scores = tokenize_tweets(args.tweet_file)
     preds = get_scores(all_tweets, affect_dict)
     writePreds2File(preds, tweet_ids, args.outputfile, args.just_numbers)
+    # if args.write_scores:
+    #     writePreds2File(all_scores, tweet_ids, args.outputfile, args.just_numbers)
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
